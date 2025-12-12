2beb39b67410ec982bd15327b621eee4500328d9
zesun <sunze1@foxmail.com>
2025-12-13 00:15:07 +0800

    add deps

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <div style={{ position: 'fixed', top: '20px', left: '20px' }}>
-        <img src={lmsLogo} style={{ width: '60px', height: '60px' }} alt="LMS logo" />
-      </div>
-    </>
+    <div style={{ position: 'fixed', top: '20px', left: '20px' }}>
+      <img
+        src={lmsLogo}
+        style={{ width: '60px', height: '60px' }}
+        alt="LMS logo"
+      />
+    </div>
   )
 }
 
